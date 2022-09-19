e39024a5a496ace269818010fb3954ecebab61b1
magallardo <gallardo@tibco.com>
2022-09-19 10:57:57 -0400

    Reformatted code

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,14 +1,14 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-    const name = core.getInput('who-to-greet');
-    console.log('Hello ${name}');
-    
-    const time = new Date();
-    core.setOutput("time", time.toTimeString());
-    
-    console.log(JSON.stringify(github, null, "\t"))
+  const name = core.getInput("who-to-greet");
+  console.log("Hello ${name}");
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
-    core.setFailed(error.message)
+  core.setFailed(error.message);
 }
